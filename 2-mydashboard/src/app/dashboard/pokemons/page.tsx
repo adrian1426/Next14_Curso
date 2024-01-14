fc7edcb4d9df211b46079699cc1142984ea0006a
adrianhh1426 <adrianhh1426@gmail.com>
2024-01-13 18:35:40 -0600

    realizando primer data fetch

--- 2-mydashboard/src/app/dashboard/pokemons/page.tsx
+++ 2-mydashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,22 @@
 
-export default function PokemonPage() {
+const getPokemons = async (limit = 20, offset = 0) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+  const data = await response.json();
+
+  return data;
+};
+
+export default async function PokemonPage() {
+
+  const pokemons = await getPokemons();
+
   return (
     <div>
       <h1>Pokemon Page</h1>
+      <hr />
+      {
+        JSON.stringify(pokemons)
+      }
     </div>
   );
 }
