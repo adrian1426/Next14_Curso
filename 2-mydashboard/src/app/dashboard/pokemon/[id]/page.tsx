616d831e2e4c46fb32d1046884022b7fd74bb549
adrianhh1426 <adrianhh1426@gmail.com>
2024-01-22 13:58:26 -0600

    extrayendo data de pokemon by id

--- 2-mydashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 2-mydashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,13 +1,36 @@
+import { PokemonById } from "../../pokemons/interfaces/pokemoInterface";
 
 interface PokemonPageProps {
   params: { id: string }
 }
 
-export default function PokemonPage(props: PokemonPageProps) {
+const getPokemonById = async (id: string): Promise<PokemonById> => {
+  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+  const response = await fetch(pokemonUrl, { cache: 'force-cache' });
+  const pokemon = await response.json();
+
+  console.log(pokemon.name)
+
+  return pokemon;
+};
+
+export const metadata = {
+  title: "Pokemon by id",
+  description: "description"
+}
+
+export default async function PokemonPage(props: PokemonPageProps) {
+
+  const pokemon = await getPokemonById(props.params.id);
 
   return (
     <div>
       <h1>pokemon: {props.params.id}</h1>
+      <hr />
+      {
+        JSON.stringify(pokemon)
+      }
     </div>
   );
 }
